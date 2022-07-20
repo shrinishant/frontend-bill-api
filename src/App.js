747bb231b00d3d33c28f3c4abd0b68c60b479351
shrinishant <nishantkumaranaspiringiitian@gmail.com>
2022-07-20 21:52:12 +0530

    Initial Commit, Basic Typical React Project and rough testing of the api with antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Table } from "antd";
 
 function App() {
+  const [bills, setBills] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [totalPages, setTotalPages] = useState(2);
+  const [loading, setLoading] = useState(false);
+
+  const getBills = async (page) => {
+
+    setLoading(true);
+    axios.get(`https://mongo-api-node.herokuapp.com/bills?page=${page}`)
+      .then((res) => {
+        // console.log(res.data.bills);
+        setBills(res.data.bills);
+        setDataSource(res.data.bills);
+        // setTotalPages(res.data.totalPages);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  }
+
+  useEffect(() => {
+    getBills();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    {
+      title: "Consumption",
+      dataIndex: "consumption",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+    },
+    {
+      title: "Bill Date",
+      dataIndex: "billDate",
+    },
+    {
+      title: "Paid Date",
+      dataIndex: "paidDate",
+    },
+  ];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nishant</h1>
+
+      <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 9,
+          // total: totalPages,
+          onChange: (page) => {
+            getBills(page);
+          },
+        }}
+      ></Table>
+    </div>
+
+      {
+        bills.map((bill, key) => {
+          return(
+            <h1 key={key}>{bill._id}</h1>
+          )
+        })
+      }
     </div>
   );
 }
